f54dde22d9626a642909183590f30aa554e8c4a7
gunelhmmtv <116768230+gunelhmmtv@users.noreply.github.com>
2022-11-30 01:59:25 +0400

    Add files via upload

---
+++ aurora/aurora/aurora/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('aurorablog.urls')),
+
+]
